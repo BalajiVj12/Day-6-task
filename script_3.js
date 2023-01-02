3c369a49f298a1c28d6a1256ffb68f811b5fe7dd
Balaji S <120628389+BalajiVj12@users.noreply.github.com>
2023-01-02 11:09:24 +0530

    Add files via upload
    Initial changes

---
+++ script_3.js
@@ -0,0 +1,52 @@
+var head1=[ 
+    {
+        
+           careerobjective : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+    },
+    {
+    summary :"i am good fun and frendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+    },
+    {
+        personalskills:"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+    }
+    ]
+     var second =[
+    {
+        Qualification:'BE, Mechanical Engineering',
+        Schoolcollege:'Adhi College Of Engineering and Technology',
+        Board:'Anna University',
+        yearofpassing:'2022',
+        percentageCGPA:'81%'
+    },
+    {
+        Qualification:'HSC',
+        Schoolcollege:'Govt Boys High School,Kilpennathur',
+        Board:'State board',
+        yearofpassing:'2018',
+        percentageCGPA:'72%'
+    },
+    {
+        Qualification:'SSLC',
+        Schoolcollege:'Govt Boys High School, Kilpennathur',
+        Board:'State board',
+        yearofpassing:'2016',
+        percentageCGPA:'87.2%'
+    }
+     ]
+    
+     var skills=
+        [ "JAVA", "SQL"]
+        var personalinfo={
+    name:"Balaji S",
+    father:"V.Sagadevan",
+    DOB:"15-06-2001",
+    Gender:"Male",
+    MAritalstatus:"single",
+    Nationality:"Indian",
+    Religion:"Hindu",
+    Mobileno: 6369389427
+        }
+    console.log(head1);
+    console.table(second)
+    console.log(skills)
+    console.table(personalinfo)
